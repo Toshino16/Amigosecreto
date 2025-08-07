b919fab2acc91931d7f1d2798830ebeb85d0f5f2
Toshino16 <hermanoguilherme4@gmail.com>
2025-08-07 19:09:01 -0300

    Mostrar resultado

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let listaDeNome = []; 
 function adicionarAmigo() {
-    let input = document.getElementById('amigo')
+    let input = document.getElementById('amigo');
     let nome = input.value.trim();
     if(nome!== '' ){
         listaDeNome.push(nome);
@@ -13,7 +13,7 @@ function sortearAmigo() {
     }else {       
         let nomeAleatorio = Math.floor(Math.random()* listaDeNome.length);
         let amigoSorteado = listaDeNome[nomeAleatorio];
-        let resultado = document.querySelector('h2');
+        let resultado = document.querySelector('ul');
         resultado.innerHTML = `Nome Sorteado é ${amigoSorteado}`;
     }
 }
